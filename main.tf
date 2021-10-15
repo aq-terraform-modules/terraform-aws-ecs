a802dae896ee33e9482ffd0dce4c573e1d70986f
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-16 03:47:42 +0700

    Init commit for dev branch

--- main.tf
+++ main.tf
@@ -1,8 +1,114 @@
+locals {
+  frontend_name = "${var.name}-frontend"
+}
+
+#################################################################################
+# CLUSTER CREATION
+#################################################################################
 resource "aws_ecs_cluster" "cluster" {
-  name = var.name
+  name               = var.name
   capacity_providers = var.capacity_providers
   setting {
-    name = containerInsights
+    name  = containerInsights
     value = var.enable_container_insights
+  }
+}
+
+#################################################################################
+# SERVICE & TASK DEFINITION CREATION
+#################################################################################
+resource "aws_ecs_task_definition" "task" {
+  family = "${local.frontend_name}"
+  cpu = var.frontend_cpu
+  memory = var.frontend_memory
+  requires_compatibilities = var.requires_compatibilities
+  network_mode = var.network_mode
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+      "cpu": ${var.frontend_cpu},
+      "memory": ${var.frontend_memory},
+      "name": "${local.frontend_name}",
+      "image": "${var.frontend_image}",
+      "networkMode": "${var.network_mode}",
+      "portMappings": [
+        {
+          "containerPort": ${var.frontend_port},
+          "hostPort": ${var.frontend_port}
+        }
+      ]
+    }
+  ]
+  TASK_DEFINITION
+}
+
+resource "aws_ecs_service" "service" {
+  name = "${local.frontend_name}"
+  cluster = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  force_new_deployment = var.force_new_deployment
+  desired_count = var.desired_count
+  launch_type = var.launch_type
+
+  deployment_circuit_breaker {
+    enabled = true
+    rollback = true
+  }
+
+  network_configuration {
+    subnets = var.subnets
+    assign_public_ip = var.assign_public_ip
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.target_group.arn
+    container_name = local.frontend_name
+    container_port = var.frontend_port
+  }
+
+  # Ignore change when task definition was updated
+  lifecycle {
+    ignore_changes = [
+      task_definition
+    ]
+  }
+}
+
+#################################################################################
+# LOADBALANCER RELATED
+#################################################################################
+resource "aws_lb_target_group" "target_group" {
+  name = local.frontend_name
+  port = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_lb_listener_rule" "rule" {
+  listener_arn = var.listener_arn
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  condition {
+    host_header {
+      values = var.frontend_domain
+    }
+  }
+}
+
+#################################################################################
+# ROUTE53
+#################################################################################
+resource "aws_route53_record" "record" {
+  zone_id = var.route53_zone_id
+  name = var.frontend_domain
+  type = "A"
+  alias {
+    name = var.lb_dns_name
+    zone_id = var.lb_zone_id
+    evaluate_target_health = var.evaluate_target_health
   }
 }
--- variables.tf
+++ variables.tf
@@ -4,10 +4,92 @@ variable "name" {
 
 variable "capacity_providers" {
   description = "Capacity providers"
-  default = "FARGATE"
+  default     = "FARGATE"
 }
 
 variable "enable_container_insights" {
   description = "Enable container insights or not"
-  default = "enabled"
+  default     = "enabled"
+}
+
+variable "frontend_cpu" {
+  description = "CPU for frontend container"
+}
+
+variable "frontend_memory" {
+  description = "Memory for frontend container"
+}
+
+variable "requires_compatibilities" {
+  description = "Task compatibilities"
+  default = "FARGATE"
+}
+
+variable "network_mode" {
+  description = "Network mode for container and task definition"
+  default = "awsvpc"
+}
+
+variable "frontend_image" {
+  description = "Frontend image to create container"
+}
+
+variable "frontend_port" {
+  description = "Container frontend port"
+}
+
+variable "frontend_domain" {
+  description = "Domain used for the frontend service"
+}
+
+variable "force_new_deployment" {
+  description = "Force new deployment when task definition get update"
+  type = bool
+  default = true
+}
+
+variable "desired_count" {
+  description = "Desired count to create the number of task inside the service"
+  default = 1
+}
+
+variable "launch_type" {
+  description = "Service launch type"
+  default = "FARGATE"
+}
+
+variable "subnets" {
+  description = "Subnet of the service"
+}
+
+variable "assign_public_ip" {
+  description = "Assign public ip to service or not"
+  type = bool
+  default = true
+}
+
+variable "vpc_id" {
+  description = "VPC ID that will associate with the LB target group"
+}
+
+variable "listener_arn" {
+  description = "LB listener ARN"
+}
+
+variable "lb_dns_name" {
+  description = "LB dns name to create the alias in route53"
+}
+
+variable "lb_zone_id" {
+  description = "Availability Zone of the LB"
+}
+
+variable "route53_zone_id" {
+  description = "Route53 id to create record for the service"
+}
+
+variable "evaluate_target_health" {
+  description = "Evaluate health of the target in the route53 alias setting"
+  type = bool
+  default = true
 }
