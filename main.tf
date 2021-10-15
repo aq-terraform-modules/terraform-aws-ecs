07ede87520d78eeab9dfd94c914d88c6869d4d2b
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-16 04:46:50 +0700

    Update

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_cluster" "cluster" {
   name               = var.name
   capacity_providers = var.capacity_providers
   setting {
-    name  = containerInsights
+    name  = "containerInsights"
     value = var.enable_container_insights
   }
 }
