1ae04b4e8be795686e043399e7cdfa6938d549fb
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-17 23:50:01 +0700

    Test local exec

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "local_file" "task_definition" {
   filename = "./frontend-task-definition.json"
 }
 
-resource "null_resource" "git1" {
+resource "null_resource" "git" {
   provisioner "local-exec" {
     command = <<-EOT
       aws codecommit put-file --repository-name ${var.name} --branch-name main --file-content file://frontend-task-definition.json --file-path frontend-task-definition. --name 'Terraform Deployment' --email 'sheid1309@gmail.com' --commit-message 'Init commit'
