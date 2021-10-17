cba88a82af9bca3eb607082ca8dfcc56dcf97815
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-17 23:30:36 +0700

    Test local exec

--- main.tf
+++ main.tf
@@ -129,11 +129,13 @@ resource "local_file" "task_definition" {
   filename = "./frontend-task-definition.json"
 }
 
-resource "null_resource" "git1" {
+resource "null_resource" "git" {
   provisioner "local-exec" {
     command = <<EOT
       "ls"
       "cat 'frontend-task-definition.json'"
+      "cat './frontend-task-definition.json'"
+      "cat ./frontend-task-definition.json"
     EOT
   }
 
