a4082832cdd909fc808ec20ee585b0f1860d4744
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-17 20:44:20 +0700

    Re-arrange files

--- main.tf
+++ main.tf
@@ -93,46 +93,6 @@ resource "aws_ecs_service" "service" {
 }
 
 #################################################################################
-# LOADBALANCER RELATED
-#################################################################################
-resource "aws_lb_target_group" "target_group" {
-  name        = local.frontend_name
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = var.vpc_id
-}
-
-
-resource "aws_lb_listener_rule" "rule" {
-  listener_arn = var.listener_arn
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
-  }
-
-  condition {
-    host_header {
-      values = ["${var.frontend_domain}.${var.parent_domain}"]
-    }
-  }
-}
-
-#################################################################################
-# ROUTE53
-#################################################################################
-resource "aws_route53_record" "record" {
-  zone_id = var.route53_zone_id
-  name    = var.frontend_domain
-  type    = "A"
-  alias {
-    name                   = var.lb_dns_name
-    zone_id                = var.lb_zone_id
-    evaluate_target_health = var.evaluate_target_health
-  }
-}
-
-#################################################################################
 # CODEPIPELINE
 #################################################################################
 resource "aws_codecommit_repository" "monitoring" {
