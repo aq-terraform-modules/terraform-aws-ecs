b00cf037dbbc6b86147b234454f4f99bef087e5a
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-17 20:05:46 +0700

    Update IAM role

--- main.tf
+++ main.tf
@@ -17,27 +17,6 @@ resource "aws_ecs_cluster" "cluster" {
 #################################################################################
 # ECS RELATED
 #################################################################################'
-data "aws_iam_policy_document" "ecs_tasks_execution_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ecs_tasks_execution_role" {
-  name               = "${var.name}-ecs-task-execution-role"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
-  role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_cloudwatch_log_group" "frontend" {
   name_prefix = var.frontend_log_group_name_prefix
 }
@@ -61,7 +40,14 @@ resource "aws_ecs_task_definition" "task" {
           "containerPort": ${var.frontend_port},
           "hostPort": ${var.frontend_port}
         }
-      ]
+      ],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "${var.frontend_log_group_name_prefix}", 
+          "awslogs-region": "${var.region}", 
+          "awslogs-stream-prefix": "ecs" 
+      }
     }
   ]
   TASK_DEFINITION
